6ba5ec7ebea3189aa464d3ecfd7df3618a34e9b7
Mehmet <44941107+mehmetext@users.noreply.github.com>
2022-03-05 18:31:15 +0300

    Create ebob_ekok.dart

---
+++ ebob_ekok.dart
@@ -0,0 +1,23 @@
+void main() {
+  ebobEkok(24, 36);
+}
+
+void ebobEkok(int sayi1, int sayi2) {
+  int ebob = 1;
+  int ekok = 0;
+
+  for (int i = 1; i <= sayi1 && i <= sayi2; i++) {
+    if (sayi1 % i == 0 && sayi2 % i == 0) {
+      ebob = i;
+    }
+  }
+
+  /*
+  ebob(sayi1, sayi2) * ekok(sayi1, sayi2) = sayi1 * sayi2
+  formülünü kullanarak EKOK'u bulabiliriz..
+  */
+  ekok = ((sayi1 * sayi2) ~/ ebob);
+
+  print("$sayi1 ve $sayi2 sayılarının EBOB'u $ebob sayısıdır.");
+  print("$sayi1 ve $sayi2 sayılarının EKOK'u $ekok sayısıdır.");
+}
